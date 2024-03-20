a99abd628155101a55164155efd855ff9c9f4ade
Santiago Zarate <santiago2001zarate@gmail.com>
2024-03-19 21:45:20 -0300

    chore: update buildCssConfig, it returns the tailwind config also

--- src/utils/buildCssConfig.ts
+++ src/utils/buildCssConfig.ts
@@ -2,32 +2,48 @@ interface Colors {
   [K: string]: string;
 }
 
-function convertToCSS(input: Colors): string {
+type ConvertToCSS = {
+  twProps: string;
+  cssProps: string;
+};
+
+function convertToCSS(input: Colors): ConvertToCSS {
   let cssProperties = "";
+  let twConfig = "";
 
   for (const key in input) {
+    twConfig += `${key}: (hsl(var(--${key}))/ <alpha-value>),\n    `;
+
     if (Object.prototype.hasOwnProperty.call(input, key)) {
       const value = input[key];
       const [hue, saturation, lightness] = value.match(/\d+/g) || [];
 
       if (hue && saturation && lightness) {
-        cssProperties += `--${key}: ${hue}deg ${saturation}% ${lightness}%;\n        `;
+        cssProperties += `--${key}: ${hue}deg ${saturation}% ${lightness}%;\n      `;
       }
     }
   }
 
-  return `
-    @layer base {
-      :root {
-        ${cssProperties}
-      }
+  const cssProps = `
+  @layer base {
+    :root {
+      ${cssProperties}
     }
-  `;
+  }
+`;
+  const twProps = `
+  colors {
+    ${twConfig}
+  }
+`;
+  return {
+    twProps,
+    cssProps,
+  };
 }
 
 const myColors: Colors = {
-  primary: "hsla(30deg, 40%, 50%)",
-  border: "hsla(30deg, 40%, 50%)",
+  primary: "hsl(30deg,40%,50%)",
+  card: "rgb(240,404, 202)",
+  border: "hsl(30deg,40%,50%)",
 };
-
-console.log(convertToCSS(myColors));
